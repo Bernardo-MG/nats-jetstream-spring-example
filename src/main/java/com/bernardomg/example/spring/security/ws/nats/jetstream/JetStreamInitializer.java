535d44bf37cdc13cf0e00a3f34e569ff334be497
Bernardo-MG <programming@bernardomg.com>
2026-04-15 14:00:02 +0200

    Cleaned up exception handling

--- src/main/java/com/bernardomg/example/spring/security/ws/nats/jetstream/JetStreamInitializer.java
+++ src/main/java/com/bernardomg/example/spring/security/ws/nats/jetstream/JetStreamInitializer.java
@@ -1,13 +1,11 @@
 
 package com.bernardomg.example.spring.security.ws.nats.jetstream;
 
-import java.io.IOException;
 import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.nats.client.JetStreamApiException;
 import io.nats.client.JetStreamManagement;
 import io.nats.client.api.StorageType;
 import io.nats.client.api.StreamConfiguration;
@@ -30,19 +28,26 @@ public final class JetStreamInitializer {
         this.jsm = Objects.requireNonNull(jsm);
     }
 
-    public void setup() throws Exception {
+    public final void setup() {
+        Boolean loaded;
+
         log.info("Initializing stream {}", stream);
 
         try {
             jsm.getStreamInfo(stream);
             log.info("Stream {} already exists", stream);
+            loaded = true;
         } catch (final Exception ex) {
+            loaded = false;
+        }
+
+        if (!loaded) {
             log.info("Stream {} not existing, adding a new instance", stream);
             loadStream();
         }
     }
 
-    private final void loadStream() throws IOException, JetStreamApiException {
+    private final void loadStream() {
         final StreamConfiguration streamConfig;
 
         streamConfig = StreamConfiguration.builder()
@@ -51,7 +56,12 @@ public final class JetStreamInitializer {
             .storageType(StorageType.File)
             .build();
 
-        jsm.addStream(streamConfig);
+        try {
+            jsm.addStream(streamConfig);
+        } catch (final Exception e) {
+            // TODO handle the exception
+            throw new RuntimeException(e);
+        }
     }
 
 }
